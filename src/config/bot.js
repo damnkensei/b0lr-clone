23f13780f525e25fb0eaa48538216d00bcee4fcc
damnkensei <122149301+damnkensei@users.noreply.github.com>
2026-07-03 13:36:23 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Playing Kovaaks",
         // Activity type number (0 = Playing).
-        type: 4,
+        type: 0,
       },
     ],
   },
