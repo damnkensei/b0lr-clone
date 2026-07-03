12f4e39281183c17865da645174265390adaaca3
damnkensei <122149301+damnkensei@users.noreply.github.com>
2026-07-03 13:31:10 +1000

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -21,10 +21,10 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [
+    activities: [0 
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "kovaaks",
         // Activity type number (0 = Playing).
         type: 0,
       },
